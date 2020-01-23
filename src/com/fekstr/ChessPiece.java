80a47ae2920ac9e2d19bf3608f8549c895e7ad26
fekstr <frithiof.ekstrom@gmail.com>
2020-01-23 12:11:39 +0100

    added coordinate class

--- src/com/fekstr/ChessPiece.java
+++ src/com/fekstr/ChessPiece.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 
 public abstract class ChessPiece {
     Player color;
-    int[] coordinates = new int[2];
+    Coordinate coordinate;
+
+    ChessPiece(Player color, Coordinate coordinate) {
+        this.color = color;
+        this.coordinate = coordinate;
+    }
 
     // Return coordinates to all valid squares
-    abstract ArrayList<int[]> checkValidMoves();
+    abstract ArrayList<int[]> getValidMoves();
 
     // Return true if move is valid and false otherwise
     abstract boolean isValidMove(int[] toCoordinates);
 
     void makeMove(int[] toCoordinates) {
         if (isValidMove(toCoordinates)) {
-             Board.handleMove(coordinates, toCoordinates);
+             Board.handleMove(coordinate, toCoordinates);
         } else {
             // BoardUI.setLabel("Invalid move")
         }
