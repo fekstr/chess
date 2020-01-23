b0d5f91627946501128f37c46ec52d88cc7950ba
fekstr <frithiof.ekstrom@gmail.com>
2020-01-23 18:40:17 +0100

    added new moves to pawn

--- src/com/fekstr/Pawn.java
+++ src/com/fekstr/Pawn.java
@@ -12,13 +12,23 @@ public class Pawn extends ChessPiece {
         ArrayList<Coordinate> validMoves = new ArrayList<>();
         ArrayList<Coordinate> movesToTest = new ArrayList<>();
 
-        movesToTest.add(new Coordinate(this.coordinate.getX(), this.coordinate.getY() + 1));
-        if (this.coordinate.getY() == 1) {
-            movesToTest.add(new Coordinate(this.coordinate.getX(), this.coordinate.getY() + 2));
+        movesToTest.add(new Coordinate(this.coordinate.getI() + 1, this.coordinate.getJ()));
+
+        if (Board.squareContainsEnemyPiece(new Coordinate(this.coordinate.getI() + 1, this.coordinate.getJ() + 1)))
+            movesToTest.add(new Coordinate(this.coordinate.getI() + 1, this.coordinate.getJ() + 1));
+
+        if (Board.squareContainsEnemyPiece(new Coordinate(this.coordinate.getI() + 1, this.coordinate.getJ() - 1)))
+            movesToTest.add(new Coordinate(this.coordinate.getI() + 1, this.coordinate.getJ() - 1));
+
+        if (this.coordinate.getI() == 1) {
+            movesToTest.add(new Coordinate(this.coordinate.getI() + 2, this.coordinate.getJ()));
         }
 
         for (Coordinate move: movesToTest) {
-            if (!Board.squareContainsOwnPiece(move) && !Board.willCreateCheck) {
+            if (!Board.isOutsideBoard(move)
+                    && !Board.squareContainsOwnPiece(move)
+                    && !Board.willCreateCheck(this.coordinate, move)
+                ) {
                 validMoves.add(move);
             }
         }
@@ -30,7 +40,9 @@ public class Pawn extends ChessPiece {
         return validMoves.contains(toCoordinate);
     }
 
-    ArrayList<Coordinate> getThreatenedSquares() {}
+    ArrayList<Coordinate> getThreatenedSquares() {
+
+    }
 
 
 }
